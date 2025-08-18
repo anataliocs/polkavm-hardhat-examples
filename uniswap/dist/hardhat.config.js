f84632a0ce767cfb789dcd0e336df6f629edbfa5
Chris Anatalio <anataliocs@gmail.com>
2025-08-18 07:20:37 -0700

    Run services in docker

---
+++ uniswap/dist/hardhat.config.js
@@ -0,0 +1,44 @@
+import "@parity/hardhat-polkadot";
+import "@acala-network/eth-rpc-adapter";
+var config = {
+    solidity: '0.8.26',
+    resolc: {
+        compilerSource: "npm",
+        settings: {
+            optimizer: {
+                enabled: true
+            }
+        }
+    },
+    networks: {
+        hardhat: {
+            polkavm: true,
+            chainId: 31337,
+            nodeConfig: {
+                nodeBinaryPath: "/Users/chrisanatalio/IdeaProjects/polkavm-hardhat-examples/westend_runtime-v1019002.compact.compressed.wasm",
+                rpcPort: 8000,
+                dev: true
+            },
+            adapterConfig: {
+                adapterBinaryPath: "/Users/chrisanatalio/IdeaProjects/polkavm-hardhat-examples/uniswap/node_modules/.bin/eth-rpc-adapter",
+                dev: true,
+                adapterPort: 8545
+            }
+        },
+        localNode: {
+            polkavm: true,
+            url: "http://127.0.0.1:8545"
+        },
+        polkadotHubTestnet: {
+            polkavm: true,
+            url: 'https://testnet-passet-hub-eth-rpc.polkadot.io',
+            chainId: 420420422
+        },
+        kusamaHub: {
+            polkavm: true,
+            url: 'https://kusama-asset-hub-eth-rpc.polkadot.io/',
+            chainId: 420420418
+        }
+    }
+};
+export default config;
--- uniswap/hardhat.config.ts
+++ uniswap/hardhat.config.ts
@@ -7,44 +7,12 @@ type PolkadotHardhatConfig
 
 const config: PolkadotHardhatConfig = {
     solidity: '0.8.26',
-    resolc: {
-        compilerSource: "npm",
-        settings: {
-            includePaths: ["contracts"],
-            optimizer: {
-                enabled: true,
-            }
-        }
-    },
     networks: {
         hardhat: {
+            throwOnCallFailures: true,
+            throwOnTransactionFailures: true,
+            docker: true,
             polkavm: true,
-            chainId: 31337,
-            nodeConfig: {
-                nodeBinaryPath: "/Users/chrisanatalio/IdeaProjects/polkavm-hardhat-examples/westend_runtime-v1019002.compact.compressed.wasm",
-                rpcPort: 8000,
-                dev: true,
-            },
-            adapterConfig: {
-                adapterBinaryPath: "/Users/chrisanatalio/IdeaProjects/polkavm-hardhat-examples/uniswap/bin/eth-rpc",
-                dev: true,
-                adapterPort: 8545,
-            },
-        },
-        localNode: {
-            polkavm: true,
-            url: `http://127.0.0.1:8545`,
-
-        },
-        polkadotHubTestnet: {
-            polkavm: true,
-            url: 'https://testnet-passet-hub-eth-rpc.polkadot.io',
-            chainId: 420420422,
-        },
-        kusamaHub: {
-            polkavm: true,
-            url: 'https://kusama-asset-hub-eth-rpc.polkadot.io/',
-            chainId: 420420418,
         },
     },
 };
